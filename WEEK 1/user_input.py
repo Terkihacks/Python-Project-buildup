327df0a8730f41f4813cda93b3ead78c56ef1c22
Terkihacks <raymondmunguti163@gmail.com>
2024-10-31 11:36:22 +0300

    Submission of week 1 Assignment

--- WEEK 1/user_input.py
+++ WEEK 1/user_input.py
@@ -5,5 +5,7 @@ age = input("How old are you ");
 # Use the input() function to ask the user for their location and store it in a variable called "location".
 location = input("Where are you from ")
 # Print out a greeting message that includes the user's name, age, and location.
-print("Hello " + name  + ", you are " + age + " years old and from " + location + ".")
+print("Hello " + name  + ", you are " + age + " years old and you from " + location + ".")
+
+print("Happy Coding !!")
 
